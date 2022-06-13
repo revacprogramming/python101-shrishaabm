dcdcce7b017c5608a20896fa49715c0af89f0fbf
shrishaabm <shrishaabysani@gmail.com>
2022-06-13 09:13:56 +0530

    done

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,12 +1,35 @@
-
-
 def get_cs():
-    a=input("Enter string")
+    a = input("enter")
     return a
-  
+
 
 def cs_to_lot(cs):
-  split=tuple(cs.split(),cs[0])
-  l=list(split)
-  return a
-  
+    d=''
+    b = tuple()
+    c = []
+    d = cs.split(";")
+    for i in d:
+        c.append(tuple(i.split("=")))
+    return c
+
+
+def lot_to_cs(cs_to_lot):
+    for i in cs_to_lot:
+        b = "=".join(i)
+        print(b, end=';')
+
+
+def main():
+    cs = get_cs()
+
+    lot = cs_to_lot(cs)  # convert connect string to list of tuples
+
+
+    lot_to_cs(lot)  # convert list of strings to connect string
+
+
+
+
+
+if __name__ == '__main__':
+    main()
