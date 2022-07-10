9d972563621b89df25710827eda103c248d196d6
shrishaabm <shrishaabysani@gmail.com>
2022-07-10 20:56:57 +0530

    done

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -2,10 +2,18 @@
 
 class Menu:
     """fill in class definition here"""
+    def __init__(self):
+        self.items=[]
+
+    def add(self,item,quant):
+        self.items.append((item,quant))
+    
+    def show(self):
+        for a,b in self.items:
+            print(a,b)
 
 
 m = Menu()  # Menu is a class
-m.add("idly", 10)
-m.add("vada". 20)
-
+m = m + ("idly", 10) + ("vada", 20)
 m.show()
+
