ef6423c315a13bcfee9fc0e97a5f179a27758198
shrishaabm <shrishaabysani@gmail.com>
2022-07-15 19:43:18 +0530

    done

--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -1,13 +1,21 @@
-
-
 class Menu:
-    """fill in class definition here"""
     def __init__(self):
-        self.items=[]
-    def add()
+        self.items = dict()
+
+    def __add__(self, item):
+        self.items[item[0]] = item[1]
+        return self
+
+    def __setitem__(self, a, b):
+        self.items[a] = b
+
+    def __str__(self):
+        f = "\n".join(" ".join((k, str(v))) for k, v in self.items.items())
+
+        return f
 
 
-m = Menu
+m = Menu()
 m["idly"] = 10
 m["vada"] = 20
 
